4938c87173d4313e8281fb6e025a3f10ba098235
kHayHoop <kennyhooper@gmail.com>
2016-07-12 20:32:41 -0700

    Learning javascript

---
+++ pages/js/js.js
@@ -0,0 +1,7 @@
+// var firstName = prompt("First Name?");
+// var lastName = prompt("Last Name?");
+// var fullName = firstName + lastName;
+//
+// var name = document.querySelector("#name");
+//
+// name.textContent(fullName);
